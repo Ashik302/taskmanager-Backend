60000cf446e6612293d98de479ad385327b65cd9
Ashik <ashikkoirala68@gmail.com>
2025-04-08 00:39:59 +0545

    checking

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import app from './src/app.js'
 
 const PORT = 5000;
+
+app.get('/', (req, res) => {
+    return res.json({message: "just checking"})
+})
+
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
